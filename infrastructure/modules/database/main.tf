7c25b79c7cafe697b9390996eb09875b2516205a
mariohoxha <mariohoxha816@gmail.com>
2023-11-01 23:08:40 +0100

    Adding new changes

--- infrastructure/modules/database/main.tf
+++ infrastructure/modules/database/main.tf
@@ -10,9 +10,21 @@ resource "aws_db_parameter_group" "pam-db" {
 resource "aws_security_group" "sec-grup" {
     vpc_id = var.vpc_id
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port = 5432
     to_port = 5432
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
